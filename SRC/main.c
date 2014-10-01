ad01d948a5ce7951fc5795707536c6b74bf06da8
ashenbrownfox <newhoopsdynasty@gmail.com>
2014-10-01 14:46:56 -0400

    Main File
    Rough template.... not working

--- SRC/main.c
+++ SRC/main.c
@@ -1,1 +1,30 @@
-/* main.c*/
+
+/*
+Ax^2 + Bx+C = 0;
+B*B-4AC >=0
+! ---------------------------------------------------
+!   Solve  Ax^2 + Bx + C = 0 given B*B-4*A*C >= 0
+!   Now, we are able to detect complex roots.
+! ---------------------------------------------------
+main.c
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	/*
+	REAL  :: a, b, c
+   REAL  :: d
+   REAL  :: root1, root2 */
+   /*
+   READ(*,*)  a, b, c
+   WRITE(*,*) 'a = ', a
+   WRITE(*,*) 'b = ', b
+   WRITE(*,*) 'c = ', c
+   WRITE(*,*)
+	printf("Hello World@ \n");
+	*/
+	return 0;
+}
