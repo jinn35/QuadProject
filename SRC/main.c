1e2e44abecf700b88ead58d1f5fb415320dedce3
ashenbrownfox <newhoopsdynasty@gmail.com>
2014-10-06 15:37:27 -0700

    Added a readme

--- SRC/main.c
+++ SRC/main.c
@@ -14,10 +14,22 @@ main.c
 
 int main(int argc, char const *argv[])
 {
+   float a,b,c,det,r1,r2;
 	/*
 	REAL  :: a, b, c
    REAL  :: d
    REAL  :: root1, root2 */
+
+   printf("Enter coefficients a, b and c: \n");
+   scanf("%f%f%f",&a,&b,&c);
+   det=b*b-4*a*c;
+
+   if(det>0)
+   {
+      r1= (-b+sqrt(det))/(2*a);
+      r2= (-b-sqrt(det))/(2*a);
+      printf("Roots are: %.2f and %.2f",r1 , r2);
+   }
    /*
    READ(*,*)  a, b, c
    WRITE(*,*) 'a = ', a
