aeae4965a5cb45700c1fd5123b2c9c4bbd75d004
jinn35 <jinniger@gmail.com>
2014-10-07 16:06:14 -0400

    finished all source files for main program, added h files, complied into test program called test5

---
+++ SRC/input.h~
@@ -0,0 +1,7 @@
+#ifndef INPUT_H_INCLUDED
+#define INPUT_H_INCLUDED
+/*^^include guards^^*/
+
+double getInput(void);
+
+#endif
---
+++ SRC/output.h
@@ -0,0 +1,9 @@
+#ifndef OUTPUT_H_INCLUDED
+#define OUTPUT_H_INCLUDED
+/*^^include guards^^*/
+
+int out1(double r1, double r2);
+int out2(double r1, double r2);
+int out3(double real, double imag);
+
+#endif
