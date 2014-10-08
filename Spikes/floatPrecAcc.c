7792f1e8cb14b69007fbf55aa8be70a2d6405f86
JAlflen <joeyalf@gmail.com>
2014-10-08 10:50:54 -0400

    Update floatPrecAcc.c

--- Spikes/floatPrecAcc.c
+++ Spikes/floatPrecAcc.c
@@ -1,12 +1,21 @@
 //spike to test precision vs accuracy of doubles and floats
+#include <math.h>
 
 int main() {
 
 float floatvar = 99.95;
 double doublevar = 99.95;
+double x = 1.6,y = 1.8, error = .7;
+
 
 printf("99.95 as float = %f\n", floatvar);   // 99.95 = 99.949997
 printf("99.95 as double = %lf\n", doublevar); // 99.95 = 99.950000
+
+if (fabs(x-y) < error)
+if ((fabs(x-y)/fabs(x) + fabs(y)) < error)
+printf("success")
+else
+printf("fail")
 
 return 0;
 }
