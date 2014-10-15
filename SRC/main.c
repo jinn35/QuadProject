4714f6a07140388ceeabd235dda97450aedc4ba7
JAlflen <joeyalf@gmail.com>
2014-10-15 01:17:13 -0400

    Update main.c

--- SRC/main.c
+++ SRC/main.c
@@ -1,11 +1,13 @@
+/*main.c*/
+/*follows GNU Coding Standards*/
+
 #include <stdio.h>
 #include "input.h"
 
 int main()
 {
-  	double a, b, c, determinant, r1,r2, real, imag;
+  	double a, b, c, determinant;
 	getInput(&a, &b, &c);
 	quad(a, b, c);
-
   	return 0;
 }
