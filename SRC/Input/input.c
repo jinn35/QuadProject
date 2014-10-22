9e878f9e9b58c6c2c920828cbf0df53b5839243c
Joey Alflen <joeyalf@gmail.com>
2014-10-22 07:16:32 -0700

    added features to protect against user input

--- SRC/Input/input.c
+++ SRC/Input/input.c
@@ -7,15 +7,62 @@ Follows C11 & GNU Coding Standards
 #include <stdio.h>
 #include "input.h"
 #include "../Quad/quad.h"
+#include <assert.h>
 
 //grabs input from user via keyboard
 void getInput(double *a, double *b, double *c)
 {
-	char buf[20];
-  	printf("Enter coefficients a, b and c seperated by a space: ");
-	if (fgets(buf, sizeof(buf), stdin) != NULL)
+	char bufa[5];
+	char bufb[5];
+	char bufc[5];
+	int num_variables;
+	int flag = 0; //0 = true, 1 = false	
+	
+// coefficient a
+	do
 	{
-		printf ("Thank you, you entered >%s<\n", buf);
-		sscanf(buf, "%lf %lf %lf", &*a, &*b, &*c);
-	}	
+  		printf("Enter coefficient a: ");
+		fgets(bufa, sizeof(bufa), stdin);
+
+		if (!isdigit(bufa[0]) && bufa[0] != ".")
+		{
+			printf("not a number!\n");
+			flag = 1;
+		}
+		else
+			flag = 0;
+	} while (flag == 1);
+	sscanf(bufa, "%lf", &*a);	
+
+// coefficient b
+	do
+	{
+  		printf("Enter coefficient b: ");
+		fgets(bufb, sizeof(bufb), stdin);
+
+		if (!isdigit(bufb[0]) && bufb[0] != ".")
+		{
+			printf("not a number!\n");
+			flag = 1;
+		}
+		else
+			flag = 0;
+	} while (flag == 1);
+	sscanf(bufb, "%lf", &*b);
+
+// coefficient C
+	do
+	{
+  		printf("Enter coefficient c: ");
+		fgets(bufc, sizeof(bufc), stdin);
+
+		if (!isdigit(bufc[0]) && bufc[0] != ".")
+		{
+			printf("not a number!\n");
+			flag = 1;
+		}
+		else
+			flag = 0;
+	} while (flag == 1);
+	sscanf(bufc, "%lf", &*c);	
 }
